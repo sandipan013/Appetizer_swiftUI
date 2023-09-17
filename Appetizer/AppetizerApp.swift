192aee8e04754b3abeaf38dba87ad04067562377
sandipan013 <sandipan013@gmail.com>
2023-09-17 23:50:20 +0530

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Sandipan Biswas on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
